84abece5796bee6c74d1889275da9858375b1d5d
Brandon Stansbury <brandon.stansbury@infospace.com>
2013-06-28 14:32:38 -0700

    change tabs to spaces

--- install_profile.sh
+++ install_profile.sh
@@ -1,7 +1,7 @@
 # Set up a ~/.bash_profile symlink to point to the profile in this repo.
 # This will allow the bash profile to automatically pick up changes.
 # Call this script on the command line as:
-#	bash install_profile.sh [-h]
+#    bash install_profile.sh [-h]
 # where -h will specify _home profile instead of the default of _work profile
 
 #!/bin/bash
@@ -11,11 +11,11 @@ PROFILE_PREFIX="$PWD/bash_profile_"
 SYM_LINK="$HOME/.bash_profile"
 
 if [ $1 == $INPUT_FLAG ] ; then
-	echo "$INPUT_FLAG flag set, using home mode."
-	SUFFIX='home'
+    echo "$INPUT_FLAG flag set, using home mode."
+    SUFFIX='home'
 else
-	echo "No $INPUT_FLAG flag set, using work mode."
-	SUFFIX='work'
+    echo "No $INPUT_FLAG flag set, using work mode."
+    SUFFIX='work'
 fi
 
 echo 'Creating bash profile symlink...'
@@ -25,9 +25,9 @@ ln -sv $PROFILE_PREFIX$SUFFIX $SYM_LINK
 RETURN_VAL=$?
 
 if [ $RETURN_VAL -ne 0 ] ; then
-	echo 'Error: no symlink created.'
+    echo 'Error: no symlink created.'
 else
-	echo 'Symlink created.'
+    echo 'Symlink created.'
 fi
 
 exit $RETURN_VAL
