ccb590bec625b2086d963f676e058e597c530373
Brandon Stansbury <brandon.stansbury@system1.com>
2021-02-18 14:22:41 -0800

    update spark version

--- zsh-work.sh
+++ zsh-work.sh
@@ -1,7 +1,7 @@
 # Environment
 unset LESS
 export PATH="/usr/local/sbin:$PATH"
-export SPARK_HOME=/usr/local/Cellar/apache-spark/2.4.4/libexec
+export SPARK_HOME=/usr/local/Cellar/apache-spark/3.0.1/libexec
 
 # pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
