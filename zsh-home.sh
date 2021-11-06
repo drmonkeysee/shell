2f17f9a14d56e6d0866dbf1523fb24f51e24db79
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-11-06 13:55:45 -0700

    more arm vs intel updates

--- zsh-home.sh
+++ zsh-home.sh
@@ -8,4 +8,4 @@ alias bbe='open -a /Applications/BBEdit.app'
 alias gcm='open -a /Applications/Google\ Chrome.app'
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 alias pyv='source venv/bin/activate'
-alias py3v="source ${HOME}/Documents/Dev/Other/py3venv/bin/activate"
+alias py3v="source ${HOME}/Documents/Dev/Tools/py3venv/bin/activate"
